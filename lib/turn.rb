f8787519bc9fa6732bf94f8817e91fd6e436dc30
Jeffrey Liu <jeffreyliu0@gmail.com>
2018-04-03 21:40:03 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -4,3 +4,6 @@ def display_board(board)
   puts " #{board[3]} , #{board[4]} , #{board[5]} "
   puts "-----------"
   puts " #{board[6]} , #{board[7]} , #{board[8]} "
+end
+
+board = Array.new(9, " ")
