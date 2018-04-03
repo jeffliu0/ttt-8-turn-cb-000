f61b4c5f99a6b13a10cba01612d90759eafa2534
Jeffrey Liu <jeffreyliu0@gmail.com>
2018-04-03 21:55:22 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -56,7 +56,7 @@ def turn(board)
   if valid_move?(converted_input)
     move(board, converted_input)
   else
-    until valid_move?(converted_input) == 0 do
+    until valid_move?(converted_input) == true do
       puts "Wrong move. Please pick the right move."
       input = gets.strip
       converted_input = input_to_index(input)
